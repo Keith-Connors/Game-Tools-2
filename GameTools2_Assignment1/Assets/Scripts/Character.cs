dfe6c751db1868399b6ecf57e0ec1625baa499a6
Keith Connors <c17507803@mydit.ie>
2018-10-08 18:26:28 +0100

    Scripts and Animations update

---
+++ GameTools2_Assignment1/Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Move(float turn, float forward)
+    {
+        m_animator.SetFloat("Turn", turn);
+        m_animator.SetFloat("Forward", forward);
+    }
+}
---
+++ GameTools2_Assignment1/Assets/Scripts/UserInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+	private float m_turn;
+	private float m_forward;
+
+	private Character m_character;
+
+	private void Start()
+	{
+		m_character = GetComponent<Character>();
+	}
+	
+	
+
+	private void FixedUpdate()
+	{
+		m_turn = Input.GetAxis("Horizontal");
+		m_forward = Input.GetAxis("Vertical");
+
+		m_character.Move(m_turn, m_forward);
+	}
+}
