9e26913e1f225d8b65a5d256b496c0d9b8db5b7c
Keith Connors <c17507803@mydit.ie>
2018-12-15 19:08:02 +0000

    .

---
+++ LWRPipeline-GameTools2/Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform JasperTransform;
+
+    public Vector3 CameraOffset;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position = JasperTransform.position - CameraOffset;
+        transform.LookAt(JasperTransform);
+    }
+}
---
+++ LWRPipeline-GameTools2/Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+     using System.Collections.Generic;
+     using UnityEngine;
+     using UnityEngine.SceneManagement;
+     
+     public class Manager : MonoBehaviour
+     {
+         [SerializeField] private float m_PlayerHealth;
+     
+         private void Start()
+         {
+     
+         }
+     
+         public void DecreaseHealth()
+         {
+             m_PlayerHealth--;
+         }
+     
+         private void CheckHealth()
+         {
+             if (m_PlayerHealth <= 0)
+             {
+                 GameOver();
+             }
+         }
+    private void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
